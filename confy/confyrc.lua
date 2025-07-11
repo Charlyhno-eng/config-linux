6021eded8f4ccc47e786d7a91f999cda997b8238
Charlyhno-eng <charly7902@gmail.com>
2025-07-11 23:41:13 +0200

    updated

--- confy/confyrc.lua
+++ confy/confyrc.lua
@@ -9,7 +9,6 @@ conky.config = {
     text_buffer_size = 2048,
     imlib_cache_size = 0,
 
-    -- Window
     own_window = true,
     own_window_type = 'normal',
     own_window_transparent = false,
@@ -22,15 +21,12 @@ conky.config = {
     minimum_width = 300,
     minimum_height = 0,
 
-    -- Background color (fond violet foncé)
     own_window_colour = '#1a1032',
 
-    -- Font
     use_xft = true,
     font = 'Hack:size=10',
     xftalpha = 0.8,
 
-    -- Colors
     default_color = '#0abdc6',
     color1 = '#ea00d9',
     color2 = '#f57800',
@@ -38,35 +34,40 @@ conky.config = {
     color4 = '#321959',
     color5 = '#0b2956',
 
-    -- Border
     draw_shades = false,
     draw_outline = false,
     draw_borders = false,
     draw_graph_borders = true,
 
-    -- Misc
     uppercase = false,
     use_spacer = 'none',
 };
 
 conky.text = [[
-${color1}CYBERPUNK SYSTEM MONITOR
+${color1}SYSTEM MONITOR
 ${color}${hr}
 
 ${color1}Time: ${color}${time %H:%M:%S}   ${color1}Uptime:${color} $uptime
 ${color1}Hostname: ${color}$nodename   ${color1}Kernel: ${color}$kernel
 
-${color1}CPU ${color}${cpu cpu0}%      ${color3}${cpubar cpu0 10,150}
-${color1}Core 1: ${color}${cpu cpu1}%  ${color2}${cpubar cpu1 8,150}
-${color1}Core 2: ${color}${cpu cpu2}%  ${color2}${cpubar cpu2 8,150}
+${color1}CPU:       ${color}${cpu cpu0}%                ${color3}${cpubar cpu0 10,200}
+${color1}Core 1:    ${color}${cpu cpu1}%                ${color2}${cpubar cpu1 8,200}
+${color1}Core 2:    ${color}${cpu cpu2}%                ${color2}${cpubar cpu2 8,200}
 
-${color1}RAM: ${color}$mem / $memmax ${color3}${membar 8,200}
-${color1}Swap: ${color}$swap / $swapmax     ${swapbar 8,200}
+${color1}Temp CPU: ${color}${execi 10 sensors | grep 'Package id 0' | awk '{print $4}'}
 
-${color1}Disk Root: ${color}${fs_used /} / ${fs_size /} ${fs_bar 8,200 /}
+${color1}RAM:       ${color}$mem/$memmax   ${color3}${membar 8,200}
+${color1}Swap:      ${color}$swap/$swapmax        ${swapbar 8,200}
+${color1}Disk Root: ${color}${fs_used /}/${fs_size /}     ${fs_bar 8,200 /}
 
-${color1}Net ↓ ${color}${downspeedf wlp0s20f3} KiB/s ${color1}↑ ${color}${upspeedf wlp0s20f3} KiB/s
-${downspeedgraph wlp0s20f3 32,150 ff0000 00ff00} ${upspeedgraph wlp0s20f3 32,150 00ff00 ff0000}
+${color1}Net ↓ ${color}${downspeedf wlp0s20f3} KiB/s ${color1}             ↑ ${color}${upspeedf wlp0s20f3} KiB/s
+${downspeedgraph wlp0s20f3 32,210 ff0000 00ff00}   ${upspeedgraph wlp0s20f3 32,210 00ff00 ff0000}
+
+${color1}IP Locale: ${color}${addr wlp0s20f3}
+
+${color1}Wi-Fi: ${color}${wireless_essid wlp0s20f3}             ${color1}Signal: ${color}${wireless_link_qual_perc wlp0s20f3}%
+
+${color1}Batterie: ${color}${execi 30 acpi -b | grep -P -o '[0-9]+(?=%)'}%                ${color1}État: ${color}${execi 30 acpi -b | awk '{print $3}' | sed 's/,//'}
 
 ${color}${hr}
 ]];
