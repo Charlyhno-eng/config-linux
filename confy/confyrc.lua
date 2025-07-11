617567167359d0efab832a6c83fb71b3f5378d50
Charlyhno-eng <charly7902@gmail.com>
2025-07-11 23:46:58 +0200

    updated

--- confy/confyrc.lua
+++ confy/confyrc.lua
@@ -16,9 +16,9 @@ conky.config = {
     own_window_argb_value = 0,
     own_window_hints = 'undecorated,sticky,skip_taskbar,skip_pager,below',
     alignment = 'top_right',
-    gap_x = 30,
-    gap_y = 30,
-    minimum_width = 300,
+    gap_x = 20,
+    gap_y = 20,
+    minimum_width = 455,
     minimum_height = 0,
 
     own_window_colour = '#1a1032',
@@ -46,9 +46,9 @@ conky.config = {
 conky.text = [[
 ${color1}SYSTEM MONITOR
 ${color}${hr}
-
 ${color1}Time: ${color}${time %H:%M:%S}   ${color1}Uptime:${color} $uptime
-${color1}Hostname: ${color}$nodename   ${color1}Kernel: ${color}$kernel
+${color1}Hostname: ${color}$nodename
+${color1}Kernel: ${color}$kernel
 
 ${color1}CPU:       ${color}${cpu cpu0}%                ${color3}${cpubar cpu0 10,200}
 ${color1}Core 1:    ${color}${cpu cpu1}%                ${color2}${cpubar cpu1 8,200}
@@ -60,14 +60,13 @@ ${color1}RAM:       ${color}$mem/$memmax   ${color3}${membar 8,200}
 ${color1}Swap:      ${color}$swap/$swapmax        ${swapbar 8,200}
 ${color1}Disk Root: ${color}${fs_used /}/${fs_size /}     ${fs_bar 8,200 /}
 
-${color1}Net ↓ ${color}${downspeedf wlp0s20f3} KiB/s ${color1}             ↑ ${color}${upspeedf wlp0s20f3} KiB/s
-${downspeedgraph wlp0s20f3 32,210 ff0000 00ff00}   ${upspeedgraph wlp0s20f3 32,210 00ff00 ff0000}
+${color1}Net ↓ ${color}${downspeedf wlp0s20f3} KiB/s ${color1}            ↑ ${color}${upspeedf wlp0s20f3} KiB/s
+${downspeedgraph wlp0s20f3 32,210 ff0000 00ff00}  ${upspeedgraph wlp0s20f3 32,210 00ff00 ff0000}
 
 ${color1}IP Locale: ${color}${addr wlp0s20f3}
 
 ${color1}Wi-Fi: ${color}${wireless_essid wlp0s20f3}             ${color1}Signal: ${color}${wireless_link_qual_perc wlp0s20f3}%
 
 ${color1}Batterie: ${color}${execi 30 acpi -b | grep -P -o '[0-9]+(?=%)'}%                ${color1}État: ${color}${execi 30 acpi -b | awk '{print $3}' | sed 's/,//'}
-
 ${color}${hr}
 ]];
