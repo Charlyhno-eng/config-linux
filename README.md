2ef7b0d1083473a629afbf7307e0207ee3e86ab4
Charlyhno-eng <charly7902@gmail.com>
2025-07-14 10:43:20 +0200

    widget updated

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 My personal Linux Mint setup with KDE Plasma and various tools.
 
-![Main Screen Preview](public/screen_principal.png)
+![Main Screen Preview](public/principal.png)
 
 ## KDE Plasma Configuration
 
@@ -33,7 +33,7 @@ Folder Shortcuts (for `~/Projets`). This widget is native to KDE Plasma.
 
 The widget to track CPU, cores, RAM, root disk and network was created via conky and is not native to KDE.
 
-![Widget](public/widget_monitor.png)
+![Widget](public/conky.png)
 
 ### Window Transparency
 
--- conky/conkyrc.lua
+++ conky/conkyrc.lua
@@ -1,5 +1,6 @@
 conky.config = {
-    background = false,
+    -- Config
+
     update_interval = 1,
     cpu_avg_samples = 2,
     net_avg_samples = 2,
@@ -9,63 +10,85 @@ conky.config = {
     text_buffer_size = 2048,
     imlib_cache_size = 0,
 
-    own_window = true,
-    own_window_type = 'normal',
-    own_window_transparent = false,
-    own_window_argb_visual = false,
-    own_window_argb_value = 0,
-    own_window_hints = 'undecorated,sticky,skip_taskbar,skip_pager,below',
-    alignment = 'top_right',
-    gap_x = 20,
-    gap_y = 20,
-    minimum_width = 455,
-    minimum_height = 0,
+    -- Windows
 
-    own_window_colour = '#1a1032',
+    own_window = true,
+    own_window_type = 'dock',
+    own_window_transparent = false,
+    own_window_argb_visual = true,
+    own_window_argb_value = 20,
+    own_window_hints = 'undecorated,sticky,skip_taskbar,skip_pager,below',
+
+    -- Placement
+
+    alignment = 'top_right',
+    gap_x = 15,
+    gap_y = 0,
+    minimum_height = 600,
+    minimum_width = 460,
+    maximum_width = 460,
+
+    -- Textual
 
     use_xft = true,
     font = 'Hack:size=10',
-    xftalpha = 0.8,
-
-    default_color = '#0abdc6',
+    xftalpha = 0.9,
+    uppercase = false,
+    use_spacer = 'none',
+    default_color = '#37d4d1',
     color1 = '#ea00d9',
-    color2 = '#f57800',
-    color3 = '#00ff00',
-    color4 = '#321959',
-    color5 = '#0b2956',
+    color2 = '#4295ed',
+
+    -- Graphical
 
     draw_shades = false,
     draw_outline = false,
     draw_borders = false,
     draw_graph_borders = true,
-
-    uppercase = false,
-    use_spacer = 'none',
+    default_shade_color = '#000000',
+    default_outline_color = '#000000',
 };
 
 conky.text = [[
-${color1}SYSTEM MONITOR
-${color}${hr}
-${color1}Time: ${color}${time %H:%M:%S}   ${color1}Uptime:${color} $uptime
-${color1}Hostname: ${color}$nodename
-${color1}Kernel: ${color}$kernel
+${color1}S Y S T E M ${hr 1} ${color}
+Hostname: ${alignr}${color2}$nodename ${color}
+Distro: ${alignr}${color2}${exec lsb_release -d | cut -f2} ${color}
+Kernel: ${alignr}${color2}$kernel ${color}
+Uptime: ${alignr}${color2}$uptime ${color}
 
-${color1}CPU:       ${color}${cpu cpu0}%                ${color3}${cpubar cpu0 10,200}
-${color1}Core 1:    ${color}${cpu cpu1}%                ${color2}${cpubar cpu1 8,200}
-${color1}Core 2:    ${color}${cpu cpu2}%                ${color2}${cpubar cpu2 8,200}
+${color1}P R O C E S S O R S ${hr 1} ${color}
+CPU Temp: ${alignr}${color2}${execi 10 sensors | grep 'Package id 0' | awk '{print $4}'} ${color}
+History:
+  CPU Core 1: ${alignr}${color2}${cpu cpu1}% ${cpubar cpu1 8,120} ${color}
+  CPU Core 2: ${alignr}${color2}${cpu cpu2}% ${cpubar cpu2 8,120} ${color}
+  CPU Core 3: ${alignr}${color2}${cpu cpu3}% ${cpubar cpu3 8,120} ${color}
+  CPU Core 4: ${alignr}${color2}${cpu cpu4}% ${cpubar cpu4 8,120} ${color}
 
-${color1}Temp CPU: ${color}${execi 10 sensors | grep 'Package id 0' | awk '{print $4}'}
+${color1}M E M O R Y ${hr 1} ${color}
+RAM:${alignr}${color2} $mem/$memmax ${membar 8,120} ${color}
 
-${color1}RAM:       ${color}$mem/$memmax   ${color3}${membar 8,200}
-${color1}Disk Root: ${color}${fs_used /}/${fs_size /}     ${fs_bar 8,200 /}
+Top Processes ${alignr} CPU%     MEM%
+  ${color2}1 - ${top name 1}${alignr}${top cpu 1}%  ${top mem 1}% ${color}
+  ${color2}2 - ${top name 2}${alignr}${top cpu 2}%  ${top mem 2}% ${color}
+  ${color2}3 - ${top name 3}${alignr}${top cpu 3}%  ${top mem 3}% ${color}
+  ${color2}4 - ${top name 4}${alignr}${top cpu 4}%  ${top mem 4}% ${color}
+  ${color2}5 - ${top name 5}${alignr}${top cpu 5}%  ${top mem 5}% ${color}
 
-${color1}Net ↓ ${color}${downspeedf wlp0s20f3} KiB/s ${color1}            ↑ ${color}${upspeedf wlp0s20f3} KiB/s
-${downspeedgraph wlp0s20f3 32,210 ff0000 00ff00}  ${upspeedgraph wlp0s20f3 32,210 00ff00 ff0000}
+${color1}D R I V E S ${hr 1} ${color}
+Root - SSD:${alignr}${color2}${fs_used /}/${fs_size /} ${fs_bar 8,120 /} ${color}
+I/O Read:${alignr}${color2}${diskio_read /dev/sda} ${cpubar 8,120} ${color}
+I/O Write:${alignr}${color2}${diskio_write /dev/sda} ${cpubar 8,120} ${color}
 
-${color1}IP Locale: ${color}${addr wlp0s20f3}
+256nvme:${alignr}${color2}${fs_used /mnt/256nvme}/${fs_size /mnt/256nvme} ${fs_bar 8,120 /mnt/256nvme} ${color}
+I/O Read:${alignr}${color2}${diskio_read /dev/nvme0n1} ${cpubar 8,120} ${color}
+I/O Write:${alignr}${color2}${diskio_write /dev/nvme0n1} ${cpubar 8,120} ${color}
 
-${color1}Wi-Fi: ${color}${wireless_essid wlp0s20f3}             ${color1}Signal: ${color}${wireless_link_qual_perc wlp0s20f3}%
+Storage:${alignr}${color2}${fs_used /mnt/storage}/${fs_size /mnt/storage} ${fs_bar 8,120 /mnt/storage} ${color}
+I/O Read:${alignr}${color2}${diskio_read /dev/sdb} ${cpubar 8,120} ${color}
+I/O Write:${alignr}${color2}${diskio_write /dev/sdb} ${cpubar 8,120} ${color}
 
-${color1}Batterie: ${color}${execi 30 acpi -b | grep -P -o '[0-9]+(?=%)'}%                ${color1}État: ${color}${execi 30 acpi -b | awk '{print $3}' | sed 's/,//'}
-${color}${hr}
+${color1}N E T W O R K ${hr 1} ${color}
+IP Address: ${alignr}${color2}${addr wlp0s20f3} ${color}
+Eth Up:${alignr}${color2}${upspeedf wlp0s20f3} ${cpubar 8,120} ${color}
+Eth Down:${alignr}${color2}${downspeedf wlp0s20f3} ${cpubar 8,120} ${color}
 ]];
