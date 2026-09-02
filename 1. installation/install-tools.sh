b8d205e837810932351a188c45e39dcaf4d0313c
Charlyhno-eng <charly7902@gmail.com>
2026-09-02 23:33:08 +0200

    some modifications in the installation script

--- 1. installation/install-tools.sh
+++ 1. installation/install-tools.sh
@@ -2,7 +2,7 @@
 
 # TODO !
 # chmod +x install-tools.sh
-# ./install-tools.sh
+# sudo ./install-tools.sh
 
 set -e
 
@@ -13,11 +13,15 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+# ------------------------------------------------------------------------
+
 echo "==> Updating packages"
 apt update && apt upgrade -y
 
 echo "==> Installing curl and git (prerequisites)"
 apt install -y curl git
+
+# ------------------------------------------------------------------------
 
 echo "==> Installing Node.js (LTS) via NodeSource"
 curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
@@ -29,21 +33,37 @@ apt install -y python3 python3-pip python3-venv
 echo "==> Installing Go"
 apt install -y golang-go
 
-echo "==> Installing C development tools (GCC, make, etc.)"
-apt install -y build-essential
+# ------------------------------------------------------------------------
 
 echo "==> Installing Zsh, bat, mpv, cava, btop, polybar"
-apt install -y zsh bat mpv cava curl git btop polybar rofi || apt install -y zsh batcat mpv cava curl git btop polybar rofi
+apt install -y zsh bat mpv cava curl btop polybar rofi || apt install -y zsh batcat mpv cava curl btop polybar rofi
 
 echo "==> Creating Rofi config directory and dumping config"
 mkdir -p ~/.config/rofi
 rofi -dump-config > ~/.config/rofi/config.rasi
 
+# ------------------------------------------------------------------------
+
 echo "==> Installing KeePassXC and SQLite Browser"
 apt install -y keepassxc sqlitebrowser
+
+echo "==> Installing dependencies for Handy"
+sudo apt install xdotool
+sudo apt install libgtk-layer-shell0
+
+echo "==> Installing Herdr"
+curl -fsSL https://herdr.dev/install.sh | sh
 
 echo "==> Setting zsh as the default shell for the current user"
 chsh -s /usr/bin/zsh "$SUDO_USER"
 
 echo "==> Installation completed."
 echo "Log out and back in to have zsh as your default shell."
+echo ""
+echo ""
+echo "--------------------------------------------------"
+echo ""
+echo ""
+echo "Handy : https://github.com/cjpais/Handy/releases"
+echo "SyncThing : https://github.com/syncthing/syncthing/releases"
+echo "Buzz : https://github.com/block/buzz"
