e423f476ca42df7ff77d1eeff5399ffecaf0aa87
Charlyhno-eng <charly7902@gmail.com>
2026-09-18 16:41:44 +0200

    docker added to the installation, some kitty params added

--- 1. installation/install-tools.sh
+++ 1. installation/install-tools.sh
@@ -79,6 +79,32 @@ echo "==> Creating Rofi config directory and dumping config"
 mkdir -p ~/.config/rofi
 rofi -dump-config > ~/.config/rofi/config.rasi
 
+# ---------------------------- Docker ----------------------------
+
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+  -o /etc/apt/keyrings/docker.asc
+
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-compose-v2
+
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
 # ----------------------------- End ------------------------------
 
 echo "==> Installation completed."
